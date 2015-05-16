c77e7f372dfc8bdc4b0eb5eef35e400408ee7967
Freibuis <freibuis@gmail.com>
2015-05-16 15:16:58 +1000

    domain aliases

--- lib/lita/handlers/google-ranking.rb
+++ lib/lita/handlers/google-ranking.rb
@@ -16,7 +16,14 @@ module Lita
       }
       def gr(response)
         # raise response.matches.inspect
-        domain = response.matches.first.first
+
+        # domain alias support
+        if config(:default_domain)[:alias] == response.matches.first.first
+          domain = config(:default_domain)[:domain]
+        else
+          domain = response.matches.first.first
+        end
+
         keyword = response.matches.first[1]
         results = Google::Search::Web.new(:query => keyword, :safety_level => :off, :gl => config(:gl), :lr => config(:lr), :cr => config(:cr), :size => :large)
         rank = nil
